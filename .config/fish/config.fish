8467e216aae038ef74ea9b9080e98108b8dd88f6
hihu <manh@hihu.tech>
2021-08-01 02:07:40 +0700

    Add config.fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,39 @@
+alias l="ls"
+alias clr="clear"
+alias cls="clear"
+
+alias sfish="source ~/.config/fish/config.fish"
+alias vfish="vim ~/.config/fish/config.fish"
+alias cfish="code ~/.config/fish/config.fish"
+
+alias y="yarn"
+alias yi="yarn add"
+alias d="yarn dev"
+alias s="yarn start"
+alias deploy="yarn server:dev"
+
+alias c="code"
+
+alias emddi="/home/hihu/dev/emddi"
+alias hihu="/home/hihu/dev/hihu"
+alias livi="/home/hihu/dev/livi"
+
+alias emddidir="cd /home/hihu/dev/emddi && ls"
+alias emddibe="cd /home/hihu/dev/emddi/be && ls"
+
+alias hihudir="cd /home/hihu/dev/hihu"
+alias lividir="cd /home/hihu/dev/livi"
+
+alias c="code"
+alias c.="code ."
+
+alias gall="git add -A && git commit -m"
+
+# peronal 
+
+alias airpod="bluetoothctl pair A1:1B:46:56:5E:DF"
+alias airpodx="bluetoothctl remove A1:1B:46:56:5E:DF"
+
+alias gall="git add -A && git commit -m"
+alias t="tmux"
+
